29e116cd5167fa7824a94cf3c3503e6712223e64
weliton <weliton.ww@hotmail.com>
2024-03-17 16:29:20 -0300

    adjust Fonts Word

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
 import { Text, View } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+
   return (
-    <View >
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      {fontsLoaded ? <Text >Hello Word!</Text> : <View/>}
     </View>
   );
 }
