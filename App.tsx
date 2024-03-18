904324d329aa3af8e9bd9ebe8a93d1a6ac2fd133
weliton <weliton.ww@hotmail.com>
2024-03-18 17:21:15 -0300

    statusBar eslint configure

--- App.tsx
+++ App.tsx
@@ -1,14 +1,29 @@
-import { Text, View } from 'react-native';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-
+/* eslint-disable camelcase */
+import { Text, View, StatusBar } from 'react-native'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
 
 export default function App() {
-  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      {fontsLoaded ? <Text >Hello Word!</Text> : <View/>}
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#202024',
+      }}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Text>Hello Word!</Text> : <View />}
     </View>
-  );
+  )
 }
-
