f74fe9928eabb8f465a18e3a4f00b44b885db32d
weliton <weliton.ww@hotmail.com>
2024-06-24 16:33:17 -0300

    screen history complete

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,15 +1,46 @@
+import { useState } from 'react'
+
+import { Heading, VStack, SectionList, Text } from 'native-base'
+
 import { HistoryCard } from '@components/HistoryCard'
 import { ScreenHeader } from '@components/ScreenHeader'
-import { VStack } from 'native-base'
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: '26.08.22',
+      data: ['Puxada frontal', 'Remada Unilateral'],
+    },
+    {
+      title: '27.08.22',
+      data: ['Puxada frontal'],
+    },
+  ])
   return (
     <VStack flex={1}>
       <ScreenHeader title="Historico de Exercícios" />
-      <HistoryCard />
-      <HistoryCard />
-      <HistoryCard />
-      <HistoryCard />
+
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <HistoryCard />}
+        px={4}
+        contentContainerStyle={
+          exercises.length === 0 && { flex: 1, justifyContent: 'center' }
+        }
+        showsVerticalScrollIndicator={false}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        ListEmptyComponent={() => (
+          <Text color="gray.200" textAlign="center">
+            Não há exercícios resgitrados {'\n'}
+            Vamos fazer exercícios hoje?
+          </Text>
+        )}
+      />
     </VStack>
   )
 }
